90735fcc41f5cfe2bd398a6fcf5edb7d21bc57d5
roberto <robertoweller78@gmail.com>
2019-05-07 20:36:30 -0300

    Esclarecendo alguns pontos

--- play_test.py
+++ play_test.py
@@ -6,9 +6,9 @@ from kivy.app import App
 class rodaVideo(App):
 	def build(self):
 		# Carrega o local do video e carrega a classe video 
-		self.video = VideoPlayer(source = 'caminho_do_video/video.mp4')
+		self.video = VideoPlayer(source = 'lind-musi-anime.mp')
 
-		return self.video # Retorna 'chama' o video a propriedade que ta a class Video()
+		return self.video # Retorna 'chama' o video e a variavel que contem a class VideoPlayer()
 
 if __name__ == '__main__':
 	# Da o estart no app, roda a bagaça, pelo app retornar o video carrega o priprio
